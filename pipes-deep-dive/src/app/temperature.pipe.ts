105456090b771278312039f78746e61528c01663
Paras Rawat <paras.rawat@iongroup.com>
2025-02-19 19:10:32 +0530

    Added Pipes-Deep-Dive

---
+++ pipes-deep-dive/src/app/temperature.pipe.ts
@@ -0,0 +1,46 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'temp',
+  standalone: true,
+})
+export class TemperaturePipe implements PipeTransform {
+  // implements to ensure use of transform
+  // a pipe requires this method
+  //   transform(value: any, ...args: []) {
+  //     return value + ' - transformed';
+  //   }
+
+  transform(
+    value: string | number,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ) {
+    let val: number;
+
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputTemp: number;
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: 'C' | 'F';
+
+    if (!outputType) {
+      symbol = inputType === 'cel' ? 'C' : 'F';
+    } else {
+      symbol = outputType === 'cel' ? 'C' : 'F';
+    }
+    return `${outputTemp.toFixed(2)} ${symbol}`;
+  }
+}
+// we can also chain pipes and the output from the first pipe is passed to the second pipe and so on
